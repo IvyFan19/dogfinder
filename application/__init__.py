c2358353ab66b2c4204ffe1315d67f08ae6f7443
Ivy Fan <xinwei.fan19@gmail.com>
2020-11-09 21:14:15 -0800

    completed info and image upload

--- application/__init__.py
+++ application/__init__.py
@@ -18,68 +18,39 @@ def post():
 @app.route('/upload', methods =['GET','POST']) 
 def upload(): 
     if request.method == 'POST':
-        # upload dog image to database
         img_file = request.files.get('file-upload')
-        print(img_file)
-        post_id = "11111"
-        response2 = requests.post(url=base_url+'/api/images/upload/'+post_id, files=img_file)
+        img_name = img_file.filename
+        location = request.form.get('location')
+        email = request.form.get('email')
+        posttype = request.form.get('posttype')
+        print(location, email, posttype, img_file)
+
+        if not img_file:
+            return 'No file uploaded.', 400
+        
+        if not (location, email, posttype):
+            return 'Not completed form.', 400
+
+        # uplaod dog info to database
+        response1 = requests.post(url=base_url+'/api/posts/create', json={
+        'location': location, 'contact': email, 'post_type': posttype})
+        
+        # test dog info uploaded 
+        data1 = response1.json()
+        status1 = data1['status']
+        post_id = data1['post_id']
+        print(' * test 1 : {}'.format(status1))
+        print(' * post_id : {}'.format(post_id))
+
+        # upload dog image to database
+        file = {'file': img_name}
+        response2 = requests.post(url=base_url+'/api/images/upload/'+post_id, files=file)
         
         # test dog image uploaded
         data2 = response2.json()
-        status_upload = data2['status']
-        image_id = data_upload['image_id']
-        print(' * test upload : {}'.format(status_upload))
+        status2 = data2['status']
+        image_id = data2['image_id']
+        print(' * test image upload : {}'.format(status2))
         print(' * image_id : {}'.format(image_id))
-
-        
-        # img_file = request.files.get('file-upload')
-        # location = request.form.get('location')
-        # email = request.form.get('email')
-        # posttype = request.form.get('posttype')
-        # print(location, email, posttype, img_file)
-
-        # if not img_file:
-        #     return 'No file uploaded.', 400
-        # # if not location or email or posttype:
-        # #     return "Not complete input", 400
-
-        # # uplaod dog info to database
-        # response1 = requests.post(url=base_url+'/api/posts/create', json={
-        # 'location': location, 'contact': email, 'post_type': posttype})
-        
-        # # test dog info uploaded 
-        # data1 = response1.json()
-        # status1 = data1['status']
-        # post_id = data1['post_id']
-        # print(' * test 1 : {}'.format(status1))
-        # print(' * post_id : {}'.format(post_id))
-
-
-        
-        
-    # response1 = requests.post(url=base_url+'/api/posts/create', json={
-    #     'location': location, 'contact': email, 'post_type': posttype})
-    # # create post test results
-    # data1 = response1.json()
-    # status1 = data1['status']
-    # post_id = data1['post_id']
-    # print(' * test 1 : {}'.format(status1))
-    # print(' * post_id : {}'.format(post_id))
-    # return render_template('index.html', response=data1)  
-
-
-# @app.route('/doginfo', methods=['GET'])
-# def getdoginfo():  
-#     ##################################################
-#     #                  create new post              #
-#     ##################################################
-#     response1 = requests.post(url=base_url+'/api/posts/create', json={
-#         'location': 'test', 'contact': 'test', 'post_type': 'lost'})
-
-#     # create post test results
-#     data1 = response1.json()
-#     status1 = data1['status']
-#     post_id = data1['post_id']
-#     print(' * test 1 : {}'.format(status1))
-#     print(' * post_id : {}'.format(post_id))
-#     return render_template('post.html', response=data1)  
+        return render_template('index.html')   
+       
