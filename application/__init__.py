ccaff26c367ffb7003e0130a5b8cf7fb1c056ce6
Ivy Fan <xinwei.fan19@gmail.com>
2020-11-11 22:04:16 -0800

    complete edit function

--- application/__init__.py
+++ application/__init__.py
@@ -14,14 +14,19 @@ def index():
         data4 = reponse4.json()
         status4 = data4['status']
         results4 = data4['result']
-        print(' * * status 4 - search: {}'.format(status4))
-        print(' * results 4 : {}'.format(results4))
-
+        # print(' * * status 4 - search: {}'.format(status4))
+        # print(' * results 4 : {}'.format(results4))
         return render_template('index.html', entities=results4)
 
     if request.method == 'GET':
-        test = 'get page'
-        return render_template('index.html', response=test)
+        reponse5 = requests.get(url=base_url+'/api/posts/search/')
+
+        data5 = reponse5.json()
+        status5 = data5['status']
+        results5 = data5['result']
+        # print(' * * status 5 - list: {}'.format(status5))
+        # print(' * results 5 : {}'.format(results5))
+        return render_template('index.html', entities=results5)
     
 # Page: post a dog
 @app.route('/post', methods=['GET'])
@@ -67,17 +72,65 @@ def upload():
         image_id = data2['image_id']
         print(' * test 2 - image upload: {}'.format(status2))
         print(' * * image_id : {}'.format(image_id))
-        return render_template('index.html')   
 
-# get a dog entity by id
-@app.route('/dog/<post_id>', methods=['GET'])
-def item(post_id):
-    reponse3 = requests.get(url=base_url+'/api/posts/get/'+post_id)
-    # get by ID test results
-    data3 = reponse3.json()
-    status3 = data3['status']
-    print(' * test 3 - get dog entity: {}'.format(status3))
-    print(' * * dog entity : {}'.format(data3))
+        reponse5 = requests.get(url=base_url+'/api/posts/search/')
+        data5 = reponse5.json()
+        results5 = data5['result']
+        return render_template('index.html', entities=results5)   
+
+
+@app.route('/getid/<post_id>', methods=['GET'])
+def getId(post_id):
+    print(post_id)
+    reponse6 = requests.get(url=base_url+'/api/posts/get/'+post_id)
+    data6 = reponse6.json()
+    status6 = data6['status']
+    result6 = data6['result']
+    print(' * test 6 - get 1 dog entity: {}'.format(status6))
+    print(' * * dog entity : {}'.format(result6))
+    return render_template('edit.html', response=result6)
+
+@app.route('/edit', methods =['GET','POST']) 
+def edit():
+   
+    if request.method == 'POST':
+        location = request.form.get('location')
+        contact = request.form.get('email')
+        posttype = request.form.get('posttype')
+        post_id = request.form.get('staticPostId')
+        img_file = request.files.get('file-upload')
+        image_id = request.form.get('staticImageId')
+        print('@@@@@@@@@@@@@@@')
+        print("image_id: ", image_id, "post_id: ", post_id, location, contact, posttype)
+        
+        # keep original image 
+        if img_file: 
+            print("update new image")
+            img_name = img_file.filename
+            content_type = 'image/jpeg'
+            file = {'file': (img_name, img_file, content_type)}
+            response = requests.post(url=base_url+'/api/images/upload/'+post_id, files=file)
+            reponse_search = requests.get(url=base_url+'/api/posts/search/')
+            data = reponse_search.json()
+            results = data['result']
+            return render_template('index.html', entities=results) 
+
+        else:
+            reponse7 = requests.post(url=base_url+'/api/posts/update', json={
+            'post_id': post_id, 'image_id': image_id, 'location': location, 
+            'contact': contact, 'breed': 'american pit', 'post_type': posttype})
+        
+            # test edit function
+            data7 = reponse7.json()
+            status7 = data7['status']
+            print(' * test 7 - edit dog entity: {}'.format(status7))
+
+            reponse5 = requests.get(url=base_url+'/api/posts/search/')
+            data5 = reponse5.json()
+            results5 = data5['result']
+            return render_template('index.html', entities=results5) 
+
+
 
 
        
